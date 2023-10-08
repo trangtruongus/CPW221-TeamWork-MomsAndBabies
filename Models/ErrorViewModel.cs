2678755bbd99ee5e38325cffab3b4a17fddd3acc
trangtruongus <ttruong3636@students.cptc.edu>
2023-10-08 15:05:12 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CPW221_MomsAndBabies.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
