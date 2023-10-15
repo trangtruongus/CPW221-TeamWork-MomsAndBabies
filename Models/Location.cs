15ab471bc4173f507dd959656dda9fb5a885a82a
trangtruongus <ttruong3636@students.cptc.edu>
2023-10-15 07:49:12 -0500

    updated enaum for States

--- Models/Location.cs
+++ Models/Location.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Xml.Linq;
 
 namespace CPW221_MomsAndBabies.Models
@@ -68,55 +69,107 @@ namespace CPW221_MomsAndBabies.Models
 
     public enum State
     {
-        Alabama,
-        Alaska,
-        Arizona,
-        Arkansas,
-        California,
-        Colorado,
-        Connecticut,
-        Delaware,
-        Florida,
-        Georgia,
-        Hawaii,
-        Idaho,
-        Illinois,
-        Indiana,
-        Iowa,
-        Kansas,
-        Kentucky,
-        Louisiana,
-        Maine,
-        Maryland,
-        Massachusetts,
-        Michigan,
-        Minnesota,
-        Mississippi,
-        Missouri,
-        Montana,
-        Nebraska,
-        Nevada,
-        NewHampshire,
-        NewJersey,
-        NewMexico,
-        NewYork,
-        NorthCarolina,
-        NorthDakota,
-        Ohio,
-        Oklahoma,
-        Oregon,
-        Pennsylvania,
-        RhodeIsland,
-        SouthCarolina,
-        SouthDakota,
-        Tennessee,
-        Texas,
-        Utah,
-        Vermont,
-        Virginia,
-        Washington,
-        WestVirginia,
-        Wisconsin,
-        Wyoming
+        [Description("Alabama")]
+        AL,
+        [Description("Alaska")]
+        AK,
+        [Description("Arkansas")]
+        AR,
+        [Description("Arizona")]
+        AZ,
+        [Description("California")]
+        CA,
+        [Description("Colorado")]
+        CO,
+        [Description("Connecticut")]
+        CT,
+        [Description("District of Columbia")]
+        DC,
+        [Description("Delaware")]
+        DE,
+        [Description("Florida")]
+        FL,
+        [Description("Georgia")]
+        GA,
+        [Description("Hawaii")]
+        HI,
+        [Description("Iowa")]
+        IA,
+        [Description("Idaho")]
+        ID,
+        [Description("Illinois")]
+        IL,
+        [Description("Indiana")]
+        IN,
+        [Description("Kansas")]
+        KS,
+        [Description("Kentucky")]
+        KY,
+        [Description("Louisiana")]
+        LA,
+        [Description("Massachusetts")]
+        MA,
+        [Description("Maryland")]
+        MD,
+        [Description("Maine")]
+        ME,
+        [Description("Michigan")]
+        MI,
+        [Description("Minnesota")]
+        MN,
+        [Description("Missouri")]
+        MO,
+        [Description("Mississippi")]
+        MS,
+        [Description("Montana")]
+        MT,
+        [Description("North Carolina")]
+        NC,
+        [Description("North Dakota")]
+        ND,
+        [Description("Nebraska")]
+        NE,
+        [Description("New Hampshire")]
+        NH,
+        [Description("New Jersey")]
+        NJ,
+        [Description("New Mexico")]
+        NM,
+        [Description("Nevada")]
+        NV,
+        [Description("New York")]
+        NY,
+        [Description("Oklahoma")]
+        OK,
+        [Description("Ohio")]
+        OH,
+        [Description("Oregon")]
+        OR,
+        [Description("Pennsylvania")]
+        PA,
+        [Description("Rhode Island")]
+        RI,
+        [Description("South Carolina")]
+        SC,
+        [Description("South Dakota")]
+        SD,
+        [Description("Tennessee")]
+        TN,
+        [Description("Texas")]
+        TX,
+        [Description("Utah")]
+        UT,
+        [Description("Virginia")]
+        VA,
+        [Description("Vermont")]
+        VT,
+        [Description("Washington")]
+        WA,
+        [Description("Wisconsin")]
+        WI,
+        [Description("West Virginia")]
+        WV,
+        [Description("Wyoming")]
+        WY
     }
 }
